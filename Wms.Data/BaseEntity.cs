587f2d94283c1fcf66858da6012f46ba0d161bd7
filip.loskovski <filip.loskovski@ASPEKT>
2019-10-18 14:07:55 +0200

    Add project files.

---
+++ Wms.Data/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Wms.Data
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Wms.Repo/Repository.cs
@@ -0,0 +1,7 @@
+﻿namespace Wms.Repo
+{
+    public class Repository
+    {
+        
+    }
+}
